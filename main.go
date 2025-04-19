102079b56d7f91c0444c103812f31ca1978cc7b8
Rahul <rahul10.singh@tataaig.com>
2025-04-19 23:07:07 +0530

    ADD: readfunc from file by offset

--- main.go
+++ main.go
@@ -10,14 +10,15 @@ func main(){
 	entry :=	kvstore.Entry{
 		TimeStamp : time.Now().Unix(),
 		Key:       "foo",
-		Value:     "bar",
+		Value:     "water",
 		KeySize:   int32(len("foo")),
-		ValueSize: int32(len("bar")),
+		ValueSize: int32(len("water")),
 	}
 
 	err:=kvstore.WriteEntry("data/store_data",entry)
 	if(err !=nil){
 	fmt.Print("ERR",err)
 	}
-
+	val,_:=kvstore.ReadEntry("data/store_data",0)
+	fmt.Print("VALUE IS  ",val)
 }
